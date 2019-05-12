18f0b11c0b857a2b106297130148ad65653def1b
ShwetaMD <m.d.shweta@gmail.com>
2019-05-12 11:14:38 +0530

    updated test

--- src/test/java/Maven/Test/AppTest.java
+++ src/test/java/Maven/Test/AppTest.java
@@ -1,38 +1,37 @@
 package Maven.Test;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.testng.Assert.assertEquals;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
 /**
- * Unit test for simple App.
+ * Hello world!
+ *
  */
-public class AppTest 
-    extends TestCase
+public class App 
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+	@Test
+	
+    public static void main( String[] args )
     {
-        super( testName );
-    }
+        //System.out.println( "Hello World!" );
+		System.setProperty("webdriver.chrome.driver", "/home/edureka/chromedriver");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--headless");
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		//driver.get("http://www.edureka.co");
+		driver.get("http://192.168.92.103:9943/index.php");
+		driver.findElementByLinkText("About Us").click();
+		//String Input = "about";
+		String text = driver.findElementById("PID-ab2-pg").getText();
+		//System.out.println(text);
+		assertEquals(text,"This is about page. Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+		System.out.println("Test Passed");
+		//assertTrue("Passed",Input.equals(text))
+		
+        driver.close();
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
     }
 }
